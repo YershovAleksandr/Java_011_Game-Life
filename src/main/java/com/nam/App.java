d662edaf31fdbe66922428aad292c493beb1c5ec
hp <namstudionsk@gmail.com>
2018-02-20 00:22:51 +0700

    Create Slick2d Hello world

--- src/main/java/com/nam/App.java
+++ src/main/java/com/nam/App.java
@@ -1,13 +1,49 @@
 package com.nam;
 
+import org.newdawn.slick.*;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 /**
  * Hello world!
  *
  */
-public class App 
+
+public class App extends BasicGame
 {
+    private static Logger log = LoggerFactory.getLogger(App.class);
+
+    public App(String gamename){
+        super(gamename);
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        log.info("Pidor");
+
+        try{
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new App("Fuck fuker fuck"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch(SlickException ex){
+            log.error("Wtf", ex);
+        }
+    }
+
+    @Override
+    public void init(GameContainer gc) throws SlickException{
+        //
+    }
+
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException{
+        //
+    }
+
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException{
+        //
+        g.drawString("Fucker Fuck", 100, 100);
     }
 }
